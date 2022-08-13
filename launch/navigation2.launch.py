c0755737d9c62e077343b09cc1e980de03c87346
Katsuyuki Itoh <itohemon@gmail.com>
2022-08-13 17:49:13 +0900

    Update launch and config

--- launch/navigation2.launch.py
+++ launch/navigation2.launch.py
@@ -59,12 +59,12 @@ def generate_launch_description():
                 'params_file': param_dir}.items(),
         ),
 
-        Node(
-            package='rviz2',
-            executable='rviz2',
-            name='rviz2',
-            arguments=['-d', rviz_config_dir],
-            parameters=[{'use_sim_time': use_sim_time}],
-            output='screen'),
+#        Node(
+#            package='rviz2',
+#            executable='rviz2',
+#            name='rviz2',
+#            arguments=['-d', rviz_config_dir],
+#            parameters=[{'use_sim_time': use_sim_time}],
+#            output='screen'),
     ])
 
