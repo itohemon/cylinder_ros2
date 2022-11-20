4711d4be9e6be1613fc8cfea95e2a29fd972f7af
Katsuyuki Itoh <itohemon@gmail.com>
2022-11-20 09:38:00 +0900

    Refinement for gazebo

--- launch/gazebo.launch.py
+++ launch/gazebo.launch.py
@@ -1,12 +1,38 @@
+#!/usr/bin/env python3
+
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 import launch
 from launch.substitutions import Command, LaunchConfiguration
 import launch_ros
-import os
 
 def generate_launch_description():
     pkg_share = launch_ros.substitutions.FindPackageShare(package='cylinder_ros2').find('cylinder_ros2')
-    default_model_path = os.path.join(pkg_share, 'urdf/cylinder.xacro')
+    default_model_path = os.path.join(pkg_share, 'urdf/cylinder_gazebo.xacro')
     default_rviz_config_path = os.path.join(pkg_share, 'rviz/cylinder.rviz')
+
+    world = os.path.join(
+        get_package_share_directory('turtlebot3_gazebo'),
+        'worlds',
+        'turtlebot3_house.world'
+    )
+    pkg_gazebo_ros = get_package_share_directory('gazebo_ros')
+    gzserver_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_gazebo_ros, 'launch', 'gzserver.launch.py')
+        ),
+        launch_arguments={'world': world}.items()
+    )
+
+    gzclient_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_gazebo_ros, 'launch', 'gzclient.launch.py')
+        )
+    )
 
     robot_state_publisher_node = launch_ros.actions.Node(
         package='robot_state_publisher',
@@ -28,7 +54,9 @@ def generate_launch_description():
     spawn_entity = launch_ros.actions.Node(
         package='gazebo_ros',
         executable='spawn_entity.py',
-        arguments=['-entity', 'cylinder', '-topic', 'robot_description'],
+        arguments=[
+            '-entity', 'cylinder',
+            '-topic', 'robot_description'],
         output='screen'
     )
     robot_localization_node = launch_ros.actions.Node(
@@ -46,10 +74,12 @@ def generate_launch_description():
                                             description='Absolute path to rviz config file'),
         launch.actions.DeclareLaunchArgument(name='use_sim_time', default_value='True',
                                             description='Flag to enable use_sim_time'),
-        launch.actions.ExecuteProcess(cmd=['gazebo', '--verbose', '-s', 'libgazebo_ros_init.so', '-s', 'libgazebo_ros_factory.so'], output='screen'),
-        joint_state_publisher_node,
+#        launch.actions.ExecuteProcess(cmd=['gazebo', '--verbose', '-s', 'libgazebo_ros_init.so', '-s', 'libgazebo_ros_factory.so'], output='screen'),
+        gzserver_cmd,
+        gzclient_cmd,
+#        joint_state_publisher_node,
         robot_state_publisher_node,
         spawn_entity,
-        robot_localization_node,
+#        robot_localization_node,
         rviz_node
     ])
